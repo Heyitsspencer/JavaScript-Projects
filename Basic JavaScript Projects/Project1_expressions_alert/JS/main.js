29a94677a37d0d1fff76f30e898b00b9ff2e4862
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-12 11:03:56 -0500

    Added page break main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,10 @@
 window.alert("Hello, World!");
 document.write('Hello, World!');
 document.write("<br>");
+document.write("<br>");
 var A= "This is a string.";
 document.write(A);
+document.write("<br>");
 document.write("<br>");
 document.write("Lisa told Bart, \"Knock it off Bart!\" Bart responded \"Eat my shorts!\"");
 document.write("<br>");
@@ -10,3 +12,7 @@ document.write("<br>");
 document.write("\"Be who you are and say what you feel,"
 +"because those who mind don\'t matter, and those who matter don\'t mind.\""
 +"-Dr Suess");
+document.write("<br>");
+document.write("<br>");
+var B="Concatenated" + "String"
+document.write(B);
