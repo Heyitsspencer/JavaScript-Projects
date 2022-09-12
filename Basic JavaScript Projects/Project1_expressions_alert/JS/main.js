3c56a11ac2f4c201378625c1b6144e5b03f01288
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-12 11:01:09 -0500

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Hello, World!");
+document.write('Hello, World!');
+document.write("<br>");
+var A= "This is a string.";
+document.write(A);
+document.write("<br>");
+document.write("Lisa told Bart, \"Knock it off Bart!\" Bart responded \"Eat my shorts!\"");
+document.write("<br>");
+document.write("<br>");
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\'t matter, and those who matter don\'t mind.\""
++"-Dr Suess");
