872898c5bc3a31fc1cf46981991936b8c407806c
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-13 12:50:46 -0500

    update index.html and main.js

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,20 @@
 function addition_Function() {
     var addition= 2 + 2;
-    document.getElementById("Math").innerHTML="2+2" + addition;
+    document.getElementById("Math").innerHTML= addition;
+}
+function subtraction_Function() {
+    var Subtraction= 5-2;
+    document.getElementById("Math1").innerHTML= Subtraction;
+}
+function division_Function() {
+    var division= 48/6;
+    document.getElementById("Math2").innerHTML= division;
+}
+function multiplication_Function() {
+    var multiplication= 6*8;
+    document.getElementById("Math3").innerHTML= multiplication;
+}
+function more_Math() {
+    var extramath= (1+2)*10/2-5;
+    document.getElementById("math4").innerHTML= extramath;
 }
