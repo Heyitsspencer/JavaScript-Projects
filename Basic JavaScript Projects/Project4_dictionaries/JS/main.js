e647e8034dc2a601e680939e80522623aa5d5fbc
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-13 15:05:57 -0500

    Debug index.html and main.js

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_Dictionary(){
+    var Pet= {
+        species: "Cat",
+        color: "White",
+        breed: "Domestic Shorthair",
+        Age: 7,
+        sound: "meow!"
+    }
+}
+document.getElementById("Dictionary").innerHTML= Pet.Age;
