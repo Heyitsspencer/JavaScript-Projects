a0060a75b80396b653bfaa471fefabe1d412f328
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-13 13:54:47 -0500

    Added comments main.js

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,35 +1,35 @@
-function addition_Function() {
+function addition_Function() { //ADDITION FUNCTION
     var addition= 2 + 2;
     document.getElementById("Math").innerHTML= addition;
 }
-function subtraction_Function() {
+function subtraction_Function() { //SUBTRACTION FUNCTION
     var Subtraction= 5-2;
     document.getElementById("Math1").innerHTML= Subtraction;
 }
-function division_Function() {
+function division_Function() { //DIVISION FUNCTION
     var division= 48/6;
     document.getElementById("Math2").innerHTML= division;
 }
-function multiplication_Function() {
+function multiplication_Function() { //MULTIPLICATION FUNCTION
     var multiplication= 6*8;
     document.getElementById("Math3").innerHTML= multiplication;
 }
-function more_Math() {
+function more_Math() { //MULTIPLE OPERATOR FUNCTION
     var extra_math= (1+2)*10/2-5;
     document.getElementById("Math4").innerHTML= extra_math;
 }
-function more_Math1() {
+function more_Math1() { //REMAINDER / MODULE FUNCTION
     var simple_math= 25%6;
     document.getElementById("Math5").innerHTML= "When you divide 25 by 6, you have a remainder of: "+ simple_math;
 }
-function negation_Operator() {
+function negation_Operator() { //NEGATION FUNCTION
     var x=10
     document.getElementById("Math6").innerHTML=-x;
 }
-var y= 10
-y++
+var y= 10 
+y++ //INCREMENTS
 document.write(y);
 var z=5
-z--
+z-- //DECREMENTS
 document.write(z);
-window.alert(Math.random()*500);
+window.alert(Math.random()*500); //RANDOM NUMBER ALERT
