52fb43b51263b9c64504d21532957f1974c0097a
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-13 10:12:41 -0500

    Update index.html and main.js

--- Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
@@ -2,3 +2,8 @@ function My_First_Function() {
     var str="This is the button text";
     document.getElementById("Button_Text").innerHTML=str;
 }
+function myFunction() {
+    var sentence= "I am learning "
+    sentence +="a lot from this book!";
+    document.getElementById("concatenate").innerHTML= sentence;
+}
