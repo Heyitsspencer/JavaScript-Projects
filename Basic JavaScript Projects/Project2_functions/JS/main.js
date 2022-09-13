fb0910addae8bebcaf683d508a8478612ab55c6d
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-13 10:53:55 -0500

    update main.js and index.html

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+function My_First_Function() {
+    var str="This is the button text";
+    document.getElementById("Button_Text").innerHTML=str;
+}
+function myFunction() {
+    var sentence= "I am learning "
+    sentence +="a lot from this book!";
+    document.getElementById("concatenate").innerHTML= sentence;
+}
+var a="7";
+var b="9";
+function mySecondFunction(a, b) {
+    var str="Thanks for clicking on me ;)";
+    document.getElementById("Multiply").innerHTML=str;
+}
