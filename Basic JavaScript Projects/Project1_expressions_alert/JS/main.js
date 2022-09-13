a9aac1b043e19c6610dbaf3ae413e6fccf4d3df2
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-13 09:37:40 -0500

    Add commenrs main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,22 +1,25 @@
-window.alert("Hello, World!");
-document.write('Hello, World!');
-document.write("<br>");
-document.write("<br>");
-var A= "This is a string.";
-document.write(A);
-document.write("<br>");
-document.write("<br>");
-document.write("Lisa told Bart, \"Knock it off Bart!\" Bart responded \"Eat my shorts!\"");
-document.write("<br>");
-document.write("<br>");
+window.alert("Hello, World!"); //Alert when page is opened/ refreshed
+document.write('Hello, World!'); //Text on screen
+document.write("<br>"); //Page break
+document.write("<br>"); //Page break
+var A= "This is a string."; //Defining variable A
+document.write(A); //Writing out variable A
+document.write("<br>"); //Page break
+document.write("<br>");//Page break
+document.write("Lisa told Bart, \"Knock it off Bart!\" Bart responded \"Eat my shorts!\"");//concatenating 
+document.write("<br>");//Page break
+document.write("<br>");//Page break
 document.write("\"Be who you are and say what you feel,"
-+"because those who mind don\'t matter, and those who matter don\'t mind.\""
++"because those who mind don\'t matter, and those who matter don\'t mind.\""//concatenating
 +"-Dr Suess");
-document.write("<br>");
-document.write("<br>");
-var B="Concatenated" + "String"
-document.write(B);
-document.write("<br>");
-document.write("<br>");
-var Dog="Murphy", Cat="Mirabelle", Lizard="Alfredo"
+document.write("<br>"); //Page break
+document.write("<br>");//Page break
+var B="Concatenated" + "String" //defining variable B (concatenated string)
+document.write(B); 
+document.write("<br>"); //Page break
+document.write("<br>");//Page break
+var Dog="Murphy", Cat="Mirabelle", Lizard="Alfredo" //Defining multiple variables in one line
 document.write(Dog)
+document.write("<br>");//Page break
+document.write("<br>");//Page break
+document.write(5+5);//Expression
