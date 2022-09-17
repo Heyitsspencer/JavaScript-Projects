b58d7649eab034fe7580bc0b72c827cff88fd378
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-17 14:46:45 -0500

    Add functions index.html and main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,2 +1,13 @@
 document.write(typeof "Word");
 document.write("10"+5);
+function my_Function() {
+    document.getElementById("Test").innerHTML=0/0;
+}
+function my_Function2() {
+    document.getElementById("Test2").innerHTML=isNaN('This is a string');
+}
+function my_Function3() {
+    document.getElementById("Test3").innerHTML=isNaN('10000');
+}
+document.write(2E310);
+document.write(-3E310);
