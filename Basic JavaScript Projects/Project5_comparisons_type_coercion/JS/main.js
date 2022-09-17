5682458eacffb77f3a5277fa93aad2bef77346c1
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-17 15:49:19 -0500

    Added comments

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,25 +1,25 @@
-document.write(typeof "Word");
-document.write("10"+5);
-function my_Function() {
+document.write(typeof "Word");  //Display data type
+document.write("10"+5); //Combining a string and a number
+function my_Function() { //NaN function
     document.getElementById("Test").innerHTML=0/0;
 }
-function my_Function2() {
+function my_Function2() { //isNan element
     document.getElementById("Test2").innerHTML=isNaN('This is a string');
 }
-function my_Function3() {
+function my_Function3() { //isNan element
     document.getElementById("Test3").innerHTML=isNaN('10000');
 }
-document.write(2E310);
-document.write(-3E310);
-document.write(1>10);
-document.write(1<10);
-console.log(2+2);
-console.log(2<0);
-document.write(10==10);
-document.write(10==12);
+document.write(2E310); //Infinity
+document.write(-3E310); //Negative infinity
+document.write(1>10);//False
+document.write(1<10);//True
+console.log(2+2); //Console
+console.log(2<0);//Console
+document.write(10==10); //True
+document.write(10==12); //False
 A=10
 B=10
-document.write(A===B);
+document.write(A===B); //===
 C=20
 D="21"
 document.write(C===D);
@@ -34,7 +34,7 @@ document.write(5<2 && 7<8);
 document.write(5>2 || 5<2);
 document.write(3>8 || 8>12);
 function not_Function() {
-    document.getElementById("Not").innerHTML=!(20>10);
+    document.getElementById("Not").innerHTML=!(20>10); //not function
 }
 function not_Function1() {
     document.getElementById("Not1").innerHTML=!(20<10);
