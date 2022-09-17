eb2b07559cbef222681c6f3125a1ca525d0a5741
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-17 16:16:59 -0500

    Added function index.html and main.js

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -4,3 +4,9 @@ function Ride_Function() {
     Can_Ride= (Height<52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML=Can_Ride+ " to ride";
 }
+function Vote_Function() {
+    var Age, Can_Vote;
+    Age= document.getElementById("Age").value;
+    Can_Vote=(Age<18) ? "You are too young":"You are old enough";
+    document.getElementById("Vote").innerHTML=Can_Vote+" to vote";
+}
