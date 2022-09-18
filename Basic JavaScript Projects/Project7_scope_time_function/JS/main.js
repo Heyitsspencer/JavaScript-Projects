954e1fcbb35eb7353b072d5f4ea425a2b3a65b7b
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-18 16:23:23 -0500

    Added local variable main.js

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,9 +1,19 @@
-var X= 10;
+var X= 10; //Global Variable
 function Add_numbers_1() {
     document.write(20 + X + "<br>");
 }
 function Add_numbers_2() {
-    document.write(X + 100);
+    document.write(X + 100 + "<br>");
 }
 Add_numbers_1();
 Add_numbers_2();
+
+function Add_numbers_3() { 
+    var Y= 10; //Local variable
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_4() { //ERROR- will not work because it is a local variable
+    document.write(Y + 100);
+}
+Add_numbers_3();
+Add_numbers_4();
