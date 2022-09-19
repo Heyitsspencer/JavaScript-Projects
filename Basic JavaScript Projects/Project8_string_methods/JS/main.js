2b8a38ca3c2db9347efb469786182ab8d644ee74
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-19 16:05:56 -0500

    Added slice method index.html main.js

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -6,3 +6,8 @@ function Sentence_Function() { //CONCATENATION ASSIGNMENT
     var whole= part1.concat(part2, part3, part4);
     document.getElementById("String").innerHTML = whole;
 }
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
