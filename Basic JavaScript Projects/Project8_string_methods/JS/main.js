4ff717e1589f72b81ba4c2de4d60d8f6341def17
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-19 16:15:06 -0500

    Added toUpperCase assignment index.html main.js

--- Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -6,8 +6,13 @@ function Sentence_Function() { //CONCATENATION ASSIGNMENT
     var whole= part1.concat(part2, part3, part4);
     document.getElementById("String").innerHTML = whole;
 }
-function slice_Method() {
+function slice_Method() { //SLICE METHOD ASSIGNMENT
     var Sentence = "All work and no play makes Johnny a dull boy.";
     var Section = Sentence.slice(27,33);
     document.getElementById("Slice").innerHTML = Section;
+}
+function uppercase_Method() { //toUppercase ASSIGNMENT
+    let text = "this text should be uppercase"
+    let result = text.toUpperCase();
+    document.getElementById("Uppercase").innerHTML = result;
 }
