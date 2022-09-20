c6466610f401fe8301f5db0c6aeb5fa4746b7434
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-20 16:05:51 -0500

    Added for loop main.js index.html

--- Basic JavaScript Projects/Project10_loops_array/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_array/JS/main.js
@@ -12,3 +12,13 @@ function Text_Length_Function() { //Text length function
     var length = text.length;
     document.getElementById("Text_Length").innerHTML = length;
 }
+//for loop:
+var Animals = ["Cat", "Dog", "Fish", "Goose", "Pigeon", "Squid"];
+var Content = "";
+var Y;
+function for_Loop() { //For Loop Function
+    for (Y = 0; Y < Animals.length; Y++) {
+        Content += Animals [Y] + "<br>";
+    }
+    document.getElementById("List_of_Animals").innerHTML = Content;
+}
