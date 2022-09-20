6f258545d65443c634f5e061db7331a7fcff9a45
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-20 15:40:13 -0500

    Created text length function index.html main.js

--- Basic JavaScript Projects/Project10_loops_array/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_array/JS/main.js
@@ -1,4 +1,4 @@
-function Count_to_Twenty() {
+function Count_to_Twenty() { //Counting Function 
     var Digit = "";
     var X = 1;
     while (X<21) {
@@ -6,4 +6,9 @@ function Count_to_Twenty() {
         X++
     }
     document.getElementById("Loop").innerHTML = Digit
+}
+function Text_Length_Function() { //Text length function
+    var text = "Spencer Allen Davis"
+    var length = text.length;
+    document.getElementById("Text_Length").innerHTML = length;
 }
