92f1e85b0c395c814f105905c428675898032e13
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-21 16:07:42 -0500

    Added animal function JavaScript_1.js

---
+++ JavaScript Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,31 @@
+function Animal_Function() {
+    var Animal_Output;
+    var Animals = document.getElementById("Animal_Input").value;
+    var Animal_String = " is a great animal!";
+    switch (Animals) {
+        case "Cat":
+            Animal_Output = "Cat" + Animal_String;
+        break;
+        case "Dog":
+            Animal_Output = "Dog" + Animal_String;
+        break;
+        case "Horse":
+            Animal_Output = "Horse" + Animal_String;
+        break;
+        case "Hippo":
+            Animal_Output = "Hippo" + Animal_String;
+        break;
+        case "Walrus":
+            Animal_Output = "Walrus" + Animal_String;
+        break;
+        case "Bird":
+            Animal_Output = "Bird" + Animal_String;
+        break;
+        case "Whale":
+            Animal_Output = "Whale" + Animal_String;
+        break;
+        default:
+            Animal_Output = "Please enter an animal exactly as written on the above list.";
+    } 
+    document.getElementById("Output").innerHTML = Animal_Output;
+}
