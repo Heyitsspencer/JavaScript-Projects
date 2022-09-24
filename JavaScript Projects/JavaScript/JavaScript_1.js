c2a3d296f5d7146ec08d9932b3a216aad8ac8c6a
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-24 10:48:40 -0500

    Add gradient in canvas JavaScript_1.js

--- JavaScript Projects/JavaScript/JavaScript_1.js
+++ JavaScript Projects/JavaScript/JavaScript_1.js
@@ -45,3 +45,15 @@ var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
 ctx.fillText("Hello World", 10, 50);
 //End <Canvas> element (Add text)
+
+//Start <Canvas> element (Add Gradient)
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
+//End <Canvas> element (Add Gradient)
