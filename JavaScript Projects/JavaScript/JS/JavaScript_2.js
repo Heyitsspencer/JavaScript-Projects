4d9a360212d0e0c8a9feefd2ed613f605fffa2bc
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-24 11:24:40 -0500

    Added form name HTML_2.html

---
+++ JavaScript Projects/JavaScript/JS/JavaScript_2.js
@@ -0,0 +1,9 @@
+//Start form validation
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x=="") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+//End form validation
