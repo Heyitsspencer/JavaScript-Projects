9e2678b5d82e7300bfc1a2b9cf7e0633bec4941f
Heyitsspencer <heyitsspencerdavis@gmail.com>
2022-09-24 12:32:48 -0500

    Added data attribute function JavaScript_3.js

---
+++ JavaScript Projects/JavaScript/JS/JavaScript_3.js
@@ -0,0 +1,6 @@
+//Start of Data Attribute Assignment
+function displayType(pet) {
+    var petType = pet.getAttribute("data-pet-type");
+    alert(petType + " is a " + pet.innerHTML + "!");
+}
+//End of Data Attribute Assignment
